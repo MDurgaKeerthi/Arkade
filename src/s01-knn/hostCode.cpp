c897fecf90540b51f59cc4eeeb47759a8960d9ae
Durga Keerthi <dkm47906@gmail.com>
2024-06-05 08:35:57 +0900

    Update hostCode.cpp

--- src/s01-knn/hostCode.cpp
+++ src/s01-knn/hostCode.cpp
@@ -1,21 +1,4 @@
-// ======================================================================== //
-// Copyright 2019-2020 Ingo Wald                                            //
-//                                                                          //
-// Licensed under the Apache License, Version 2.0 (the "License");          //
-// you may not use this file except in compliance with the License.         //
-// You may obtain a copy of the License at                                  //
-//                                                                          //
-//     http://www.apache.org/licenses/LICENSE-2.0                           //
-//                                                                          //
-// Unless required by applicable law or agreed to in writing, software      //
-// distributed under the License is distributed on an "AS IS" BASIS,        //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
-// See the License for the specific language governing permissions and      //
-// limitations under the License.                                           //
-// ======================================================================== //
-
-// This program renders the recursive Sierpinski tetrahedron to a given depth.
-// The code demonstrates how to create nested instances.
+// All rights reserved 
 
 // public owl API
 #include <owl/owl.h>
